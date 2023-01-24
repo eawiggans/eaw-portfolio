08ece9a414459bff3c53e970997cdcd47111c074
Emma Wiggans <ea.wiggans@gmail.com>
2023-01-24 17:17:00 -0500

    made initial render contact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import BucketList from './components/BucketList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import Navigation from './components/Navigation';
+// import Project from './components/Project';
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div className="portfolio-app">
+      <div className='container'><Header /></div>
+
+
+
+      <div className='container'><Footer /></div>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+  // All functional components must have a return method that contains JSX.
+  // We return all the JSX inside a parent element with a className of "container".
+  return (
+    <div className="container">
+      <h3>Find out more:</h3>
+      <ul className="parent">
+        <li>Github Profile</li>
+        <li>Linked In Profile</li>
+      </ul>
+    </div>
+  );
+}
