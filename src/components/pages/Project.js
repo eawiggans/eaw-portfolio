a8f448f5d50b0f4fcbd9d8b23e697cc4e0b1295b
Emma Wiggans <ea.wiggans@gmail.com>
2023-01-26 14:08:27 -0500

    added code to work expamples and projects

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,10 +4,13 @@ import weather from '../../assets/weathercrop.png';
 
 function projectCard() {
     return (
-        
-        <div className='container'>
-            <div className='container'>
-                <img src={weather} alt='Screenshot of weather tracking app' /><div className='work-title'><a href='https://eawiggans.github.io/weather-tracker/'><h3>Weather Tracker App</h3></a><p><a href='https://github.com/eawiggans/weather-tracker'>Github Repo</a></p></div></div>
+
+            <div className='container project-card'>
+                <img src={weather} alt='Screenshot of weather tracking app' />
+                <div className='work-title'><a href='https://eawiggans.github.io/weather-tracker/'><p className='main-link'>Weather Tracker App</p></a>
+                <p><a href='https://github.com/eawiggans/weather-tracker'>Github Repo</a></p>
+                </div>
+
         </div>
     )
 }
--- src/components/pages/WorkExamples.js
+++ src/components/pages/WorkExamples.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Project from './Project'
+import Project from './Project';
+import './WorkExamples.css';
 
 export default function WorkExamples() {
   return (
     <div>
       <h2>Work Examples</h2>
-      <div><Project /></div>
+      <div className='container work-container'>
+        <Project /><Project /></div>
+        <div className='container work-container'>
+        <Project /><Project /></div>
+        <div className='container work-container'>
+        <Project /><Project /></div>
     </div>
   );
 }
